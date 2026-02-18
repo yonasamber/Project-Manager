40eec2c14e6b5611c2c6eaa04ed9b597e9b71de0
yonasamber <yonasamber@gmail.com>
2026-02-18 11:30:26 +0300

    generate prisma

--- server/configs/prisma.js
+++ server/configs/prisma.js
@@ -1,7 +1,7 @@
-import 'dotenv/config'
-import { PrismaClient } from './generated/prisma'
-import { PrismaNeon } from '@prisma/adapter-neon'
+import "dotenv/config";
+import { PrismaClient } from "@prisma/client";
+import { PrismaNeon } from "@prisma/adapter-neon";
 const adapter = new PrismaNeon({
-  connectionString: process.env.DATABASE_URL!,
-})
-export const prisma = new PrismaClient({ adapter })
+  connectionString: process.env.DATABASE_URL,
+});
+export const prisma = new PrismaClient({ adapter });
